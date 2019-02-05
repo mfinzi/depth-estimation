fixed normalization bug for the permutohedral lattic, now works with dense crf

crf/crf.py
@@ -5,7 +5,7 @@ import numpy as np
 def gaussian_weights(f):
     """[f] n x c flattened reference 'image' to filter by"""
     I = torch.eye(f.shape[0]).to(f.device)
-    W = torch.exp(-((f[None,:,:] - f[:,None,:])**2).sum(-1)/2)-I
+    W = torch.exp(-((f[None,:,:] - f[:,None,:])**2).sum(-1))-I
     D = W@torch.ones(f.shape[0]).to(f.device)
     D_invsqrt = torch.diag(1/torch.sqrt(D))
     W_normalized = D_invsqrt@(W-I)@D_invsqrt
@@ -13,7 +13,7 @@ def gaussian_weights(f):
 
 def gaussian_weights_u(f):
     I = torch.eye(f.shape[0]).to(f.device)
-    W = torch.exp(-((f[None,:,:] - f[:,None,:])**2).sum(-1)/2)-I
+    W = torch.exp(-((f[None,:,:] - f[:,None,:])**2).sum(-1))-I
     return W
 
 def lazy_W(f):

crf/gaussian_matrix.py
@@ -1,10 +1,10 @@
 import torch
 import torch.nn as nn
 from torch.utils.cpp_extension import load
-
+import os
 import crf.lsh
 
-lattice = load(name="lattice",sources=["../crf/lattice/lite/lattice.cpp"])
+lattice = load(name="lattice",sources=[os.path.expanduser("~/depth-estimation/crf/lattice/lite/lattice.cpp")])
 latticefilter = lattice.filter
 
 class LatticeGaussian(nn.Module):
@@ -14,8 +14,8 @@ class LatticeGaussian(nn.Module):
 
     def __matmul__(self,U):
         return self(U)
-    def __rmatmul__(self,U):
-        return self(U)
+    # def __rmatmul__(self,U):
+    #     return self(U)
 
     def forward(self,U):
         return latticefilter(U,self.ref) - U
@@ -31,8 +31,8 @@ class LSHGaussian(nn.Module):
 
     def __matmul__(self,U):
         return self(U)
-    def __rmatmul__(self,U):
-        return self(U)
+    # def __rmatmul__(self,U):
+    #     return self(U)
 
     def forward(self,U):
         return crf.lsh.filter(U,self.ref,5,5,30) - U