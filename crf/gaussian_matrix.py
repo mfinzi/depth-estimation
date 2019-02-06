7bd4c9896a59616eb96b01a33447c8d796066f06
Marc Anton Finzi <mfinzi@hmc.edu>
2019-02-06 18:45:32 -0500

    updates to the backwards function

--- crf/gaussian_matrix.py
+++ crf/gaussian_matrix.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 from torch.utils.cpp_extension import load
 import os
-import crf.lsh
+import crf
 from torch.autograd import Function
 
 
@@ -26,7 +26,8 @@ class LatticeFilter(Function):
     @staticmethod
     def forward(ctx, source, reference):
         # Typical runtime of O(nd^2 + n*L), Worst case O(nd^2 + n*L*d)
-        # assert source and reference are (b x) n x L and n x d respectively
+        assert source.shape[0] == reference.shape[0], \
+            "Incompatible shapes {}, and {}".format(source.shape,reference.shape)
         ctx.save_for_backward(source,reference) # TODO: add batch compatibility
         return latticefilter(source,reference)
     @staticmethod
@@ -46,7 +47,7 @@ class LatticeFilter(Function):
             sf = src_and_ref = src[...,None]*ref[...,None,:] # n x L x d
             src_and_ref_flat = src_and_ref.view(src_and_ref.shape[:-2]+(L*d,))
             #n x (L+Ld+L+Ld):   n x L       n x Ld     n x L   n x Ld 
-            all_ = torch.cat([grad_source,grad_and_ref_flat,src,src_and_ref_flat],dim=-1)
+            all_ = torch.cat([g,grad_and_ref_flat,src,src_and_ref_flat],dim=-1)
             filtered_all = latticefilter(all_,ref)
             [wg,wgf,ws,wsf] = torch.split(filtered_all,[L,L*d,L,L*d],dim=-1)
             # has shape n x d
@@ -57,37 +58,26 @@ class LatticeFilter(Function):
 
 
 
+# class LSHGaussian(nn.Module):
+#     def __init__(self,ref):
+#         super().__init__()
+#         self.ref = ref
+
+#     def __matmul__(self,U):
+#         return self(U)
+#     # def __rmatmul__(self,U):
+#     #     return self(U)
+
+#     def forward(self,U):
+#         return crf.lsh.filter(U,self.ref,5,5,30) - U
+
+#     def backward(self,*args,**kwargs):
+#         raise NotImplementedError
 
 
-
-    @staticmethod
-    def backward(ctx,grad_output):
-        input, weight, bias = ctx.saved_tensors
-        grad_input = grad_weight = grad_bias = None
-        if ctx.needs_input_grad[0]:
-            grad_input = grad_output.mm(weight)
-        if ctx.needs_input_grad[1]:
-            grad_weight = grad_output.t().mm(input)
-        if bias is not None and ctx.needs_input_grad[2]:
-            grad_bias = grad_output.sum(0).squeeze(0)
-
-        return grad_input, grad_weight, grad_bias
-
-
-
-
-class LSHGaussian(nn.Module):
-    def __init__(self,ref):
-        super().__init__()
-        self.ref = ref
-
-    def __matmul__(self,U):
-        return self(U)
-    # def __rmatmul__(self,U):
-    #     return self(U)
-
-    def forward(self,U):
-        return crf.lsh.filter(U,self.ref,5,5,30) - U
-
-    def backward(self,*args,**kwargs):
-        raise NotImplementedError
+if __name__=="__main__":
+    from torch.autograd import gradcheck
+    ref = torch.rand(1000,5,dtype=torch.double,requires_grad=True)
+    src = torch.rand(1000,2,dtype=torch.double,requires_grad=True)
+    test = gradcheck(LatticeFilter,(ref,src),eps=1e-6,atol=1e-4)
+    print(test)
